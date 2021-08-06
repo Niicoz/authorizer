11458cb149fd70a1efcfe983e572ad397acaeb83
marcos.wilson <marcoswilsonpf@hotmail.com>
2021-08-06 13:25:35 -0300

    Adding purchases by category tests

--- test/authorizer/logic_test.clj
+++ test/authorizer/logic_test.clj
@@ -20,3 +20,11 @@
     (let [purchases (s/as-queue []), purchase {:client-cpf "123456" :date "14/07/2020", :value 500, :establishment "Piticas" :category "Roupas"}]
       (is (thrown? clojure.lang.ExceptionInfo
                    (add-purchase purchases purchase)))))){:id 2, :client-cpf "123456" :date "14/07/2020", :value 500, :establishment "Piticas" :category "Roupas"}
+
+(deftest purchases-by-category-test
+  (testing "testing purchases by category"
+    (let [purchases [{:id 1, :client-cpf "123456" :date "14/07/2020", :value 500, :establishment "Piticas" :category "Roupas"},
+                     {:id 2, :client-cpf "123456" :date "14/07/2020", :value 100, :establishment "Piticas" :category "Roupas"},
+                     {:id 3, :client-cpf "123456" :date "14/07/2020", :value 100, :establishment "Riot Games" :category "Games"}]]
+      (is (= [{:category "Roupas", :total-value 600}, {:category "Games", :total-value 100}]
+             (purchases-by-category purchases))))))
